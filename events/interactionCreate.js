18eb623805c1c35752c53fbb0116d4377da0ca00
LaureiVarju <mdilzell@yahoo.com>
2022-02-16 07:59:50 -0500

    event handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,24 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+	
+
+		client.on('interactionCreate', async interaction => {
+	
+			if (!interaction.isCommand()) return;
+		
+			const command = client.commands.get(interaction.commandName);
+		
+			if (!command) return;
+		
+			try {
+				console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		});
 	},
 };
 
